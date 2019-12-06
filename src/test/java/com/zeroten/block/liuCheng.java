42663706d90588d2e10fa5e140f76f80c1b7064f
yhy <1010991539@qq.com>
2019-12-06 10:54:29 +0800

    测试方法快作用域

---
+++ src/test/java/com/zeroten/block/liuCheng.java
@@ -0,0 +1,20 @@
+package com.zeroten.block;
+
+import org.testng.annotations.Test;
+
+public class liuCheng {
+
+    @Test
+    public void testBlock(){
+        int n1 = 100;
+        {
+            //int n1 = 100;  //外面已经定义了n1
+            System.out.println(n1);
+            int n2 = 101;
+            System.out.println(n2);
+        }
+        //System.out.println(n2); //块的作用域只在块中,在块的外部无法访问
+        int n2 = 102;
+        System.out.println(n2);
+    }
+}
